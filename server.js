1571b66941ed929df5a154d4922c706f7ee536e0
Gerardo Gonzalez Flores <71337842+traitor0426@users.noreply.github.com>
2024-04-11 11:35:40 -0600

    Update server.js

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ app.delete('/videojuegos/:id', (req, res) => {
 
 app.get('/sumaTotal', (req, res) => {
     const data = readData();
-    const totalSum = data.videojuegos.reduce((sum, videojuego) => sum + videojuego.precio, 0);
+    const totalSum = data.videojuegos.reduce((sum, videojuego) => sum + videojuegos.Precio, 0);
     res.json({ result: totalSum });
 });
 
